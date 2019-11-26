f6a51e0d4bd28d52a864441dfa38ed0037c0e04a
Junkang <yuanjunkang@gmail.com>
2019-11-26 18:10:01 +0800

    upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aiomail",
-    version="0.0.1",
+    version="0.0.2",
     author="fxdmhtt",
     author_email="yuanjunkang@gmail.com",
     description="easy and async e-mail package",
